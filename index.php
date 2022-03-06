13ac68d49c23a7fd97877fd0e450d5ef40d40ce8
ranggaauliap <ranggaaulia025@gmail.com>
2022-03-06 12:07:15 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae quia placeat mollitia. Quos blanditiis corporis alias, beatae magnam praesentium atque esse consequuntur asperiores, odio inventore ad suscipit, mollitia vitae! Pariatur.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
